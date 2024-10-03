053915d81d866081ca68060d56ad055bacc649ea
budOs15 <os_b15@outlook.it>
2024-10-03 06:08:27 +0200

    13th Commit: - docker-compose updated with springboot-app ports. - componentscan added to application file.

--- plox-backend/src/main/java/com/budos15/plox/PloxApplication.java
+++ plox-backend/src/main/java/com/budos15/plox/PloxApplication.java
@@ -3,8 +3,17 @@ package com.budos15.plox;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {
+	"com.budos15.plox.plox-backend.controller.UserController"
+	"com.budos15.plox.plox-backend.controller.VendorController"
+	"com.budos15.plox.plox-backend.controller.ProductController"
+	"com.budos15.plox.plox-backend.controller.OrderController"
+	"com.budos15.plox.plox-backend.controller.CartController"
+})
 public class PloxApplication {
 
 	public static void main(String[] args) {
