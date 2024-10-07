d4c8764defd6c8945336a445812172a70c3f106a
budOs15 <os_b15@outlook.it>
2024-10-08 01:39:43 +0200

    14th Commit: fixed synthax on PloxApplication.java

--- plox-backend/src/main/java/com/budos15/plox/PloxApplication.java
+++ plox-backend/src/main/java/com/budos15/plox/PloxApplication.java
@@ -8,10 +8,10 @@ import org.springframework.context.annotation.ComponentScans;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {
-	"com.budos15.plox.plox-backend.controller.UserController"
-	"com.budos15.plox.plox-backend.controller.VendorController"
-	"com.budos15.plox.plox-backend.controller.ProductController"
-	"com.budos15.plox.plox-backend.controller.OrderController"
+	"com.budos15.plox.plox-backend.controller.UserController",
+	"com.budos15.plox.plox-backend.controller.VendorController",
+	"com.budos15.plox.plox-backend.controller.ProductController",
+	"com.budos15.plox.plox-backend.controller.OrderController",
 	"com.budos15.plox.plox-backend.controller.CartController"
 })
 public class PloxApplication {
